5d7e801b008174a86600d31f5968417bf3b769b9
ROOter <ralph@rfactory.org>
2021-12-07 17:49:28 -0800

    atcmd fix
    atcmd fix

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/atcmd.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/atcmd.sh
@@ -7,10 +7,12 @@ CURRMODEM=$(uci get modem.general.miscnum)
 COMMPORT="/dev/ttyUSB"$(uci get modem.modem$CURRMODEM.commport)
 
 M2=$(echo "$ATCMDD" | sed -e "s#~#\"#g")
-COPS="at+cops=?"
+COPS="+cops=?"
+QOPS="+qops?"
 M3=$(echo "$M2" | awk '{print tolower($0)}')
-if `echo ${M3} | grep "${COPS}" 1>/dev/null 2>&1`
-then
+if `echo ${M3} | grep "${COPS}" 1>/dev/null 2>&1`; then
+	export TIMEOUT="120"
+elif `echo ${M3} | grep "${QOPS}" 1>/dev/null 2>&1`; then
 	export TIMEOUT="120"
 else
 	export TIMEOUT="5"
