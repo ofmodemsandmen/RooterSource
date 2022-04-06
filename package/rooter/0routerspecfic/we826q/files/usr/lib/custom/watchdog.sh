3d228e3ce9b5e22928979190f662ea10c7435fde
ROOter <ralph@rfactory.org>
2022-04-06 15:45:25 -0700

    WE826Q fix
    WE826Q fix

--- package/rooter/0routerspecfic/we826q/files/usr/lib/custom/watchdog.sh
+++ package/rooter/0routerspecfic/we826q/files/usr/lib/custom/watchdog.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 #for example WE825-Q watchdog gpio is 2
-wd_gpio="2"
 
+gpio_4g=1
+echo $gpio_4g > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio${gpio_4g}/direction
+echo 1  > /sys/class/gpio/gpio${gpio_4g}/value
+	
+wd_gpio="2"
 echo $wd_gpio > /sys/class/gpio/export
 echo out > /sys/class/gpio/gpio$wd_gpio/direction
 
