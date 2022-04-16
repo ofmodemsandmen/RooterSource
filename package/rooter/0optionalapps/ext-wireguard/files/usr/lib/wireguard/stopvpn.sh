b7034f20c41e4237fdcfb50ba2e7caaa8786eacb
ROOter <ralph@rfactory.org>
2022-04-16 10:12:05 -0700

    Wireguard start
    Wireguard start

--- package/rooter/0optionalapps/ext-wireguard/files/usr/lib/wireguard/stopvpn.sh
+++ package/rooter/0optionalapps/ext-wireguard/files/usr/lib/wireguard/stopvpn.sh
@@ -11,9 +11,25 @@ SERVE=$(uci get wireguard."$WG".client)
 if [ $SERVE = "0" ]; then
 	ifdown wg1
 	uci set wireguard.settings.server="0"
+	uci delete network.wg1
+	uci set network.wg1=interface
+	uci set network.wg1.proto="wireguard"
+	uci set network.wg1.auto="0"
+	uci set network.wg1.private_key=""
+	uci set network.wg1.listen_port=""
+	uci add_list network.wg1.addresses=""
+	uci commit network
 else
 	ifdown wg0
 	uci set wireguard.settings.client="0"
+	uci delete network.wg0
+	uci set network.wg0=interface
+	uci set network.wg0.proto="wireguard"
+	uci set network.wg0.auto="0"
+	uci set network.wg0.private_key=""
+	uci set network.wg0.listen_port=""
+	uci add_list network.wg0.addresses=""
+	uci commit network
 fi
 UDP=$(uci get wireguard."$WG".udptunnel)
 if [ $UDP = 1 ]; then
