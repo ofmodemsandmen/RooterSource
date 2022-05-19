7ec8ec59cd34eed5a2bf8ec201ccecc06d3a6ea6
ROOter <ralph@rfactory.org>
2022-05-19 11:23:38 -0700

    celldata
    celldata

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/signal/celldata.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/signal/celldata.sh
@@ -51,71 +51,76 @@ COPS_MNC=" "$COPS_MNC
 
 OX=$(echo "${OX//[ \"]/}")
 
-REGV=$(echo "$OX" | grep -o "+C5GREG:2,[0-9],[A-F0-9]\{2,6\},[A-F0-9]\{5,10\}")
+CID=""
+CID5=""
+RAT=""
+REGV=$(echo "$OX" | grep -o "+C5GREG:2,[0-9],[A-F0-9]\{2,6\},[A-F0-9]\{5,10\},[0-9]\{1,2\}")
+if [ -n "$REGV" ]; then
+	LAC5=$(echo "$REGV" | cut -d, -f3)
+	LAC5=$LAC5" ($(printf "%d" 0x$LAC5))"
+	CID5=$(echo "$REGV" | cut -d, -f4)
+	CID5L=$(printf "%010X" 0x$CID5)
+	RNC5=${CID5L:1:6}
+	RNC5=$RNC5" ($(printf "%d" 0x$RNC5))"
+	CID5=${CID5L:7:3}
+	CID5="Short $(printf "%X" 0x$CID5) ($(printf "%d" 0x$CID5)), Long $(printf "%X" 0x$CID5L) ($(printf "%d" 0x$CID5L))"
+	RAT=$(echo "$REGV" | cut -d, -f5)
+fi
+REGV=$(echo "$OX" | grep -o "+CEREG:2,[0-9],[A-F0-9]\{2,4\},[A-F0-9]\{5,8\}")
+REGFMT="3GPP"
+if [ -z "$REGV" ]; then
+	REGV=$(echo "$OX" | grep -o "+CEREG:2,[0-9],[A-F0-9]\{2,4\},[A-F0-9]\{1,3\},[A-F0-9]\{5,8\}")
+	REGFMT="SW"
+fi
 if [ -n "$REGV" ]; then
 	LAC=$(echo "$REGV" | cut -d, -f3)
-	LAC=$(printf "%06X" 0x$LAC)
-	CID=$(echo "$REGV" | cut -d, -f4)
-	CID=$(printf "%010X" 0x$CID)
-	RNC=${CID:1:6}
-	CID=${CID:7:3}
-	RNC="-"
-else
-	REGV=$(echo "$OX" | grep -o "+CEREG:2,[0-9],[A-F0-9]\{2,4\},[A-F0-9]\{5,8\}")
-	REGFMT="3GPP"
-	if [ -z "$REGV" ]; then
-		REGV=$(echo "$OX" | grep -o "+CEREG:2,[0-9],[A-F0-9]\{2,4\},[A-F0-9]\{1,3\},[A-F0-9]\{5,8\}")
-		REGFMT="SW"
+	LAC=$(printf "%04X" 0x$LAC)" ($(printf "%d" 0x$LAC))"
+	if [ $REGFMT = "3GPP" ]; then
+		CID=$(echo "$REGV" | cut -d, -f4)
+	else
+		CID=$(echo "$REGV" | cut -d, -f5)
 	fi
+	CIDL=$(printf "%08X" 0x$CID)
+	RNC=${CIDL:1:5}
+	RNC=$RNC" ($(printf "%d" 0x$RNC))"
+	CID=${CIDL:6:2}
+	CID="Short $(printf "%X" 0x$CID) ($(printf "%d" 0x$CID)), Long $(printf "%X" 0x$CIDL) ($(printf "%d" 0x$CIDL))"
+
+else
+	REGV=$(echo "$OX" | grep -o "+CREG:2,[0-9],[A-F0-9]\{2,4\},[A-F0-9]\{2,8\}")
 	if [ -n "$REGV" ]; then
 		LAC=$(echo "$REGV" | cut -d, -f3)
-		LAC=$(printf "%04X" 0x$LAC)
-		if [ $REGFMT = "3GPP" ]; then
-			CID=$(echo "$REGV" | cut -d, -f4)
-		else
-			CID=$(echo "$REGV" | cut -d, -f5)
-		fi
-		CID=$(printf "%08X" 0x$CID)
-		RNC=${CID:1:5}
-		CID=${CID:6:2}
-	else
-		REGV=$(echo "$OX" | grep -o "+CREG:2,[0-9],[A-F0-9]\{2,4\},[A-F0-9]\{2,8\}")
-		if [ -n "$REGV" ]; then
-			LAC=$(echo "$REGV" | cut -d, -f3)
-			CID=$(echo "$REGV" | cut -d, -f4)
-			if [ ${#CID} -gt 4 ]; then
-				LAC=$(printf "%04X" 0x$LAC)
-				CID=$(printf "%08X" 0x$CID)
-				RNC=${CID:1:3}
-				CID=${CID:4:4}
-			else
-				RNC="-"
-			fi
+		CID=$(echo "$REGV" | cut -d, -f4)
+		if [ ${#CID} -gt 4 ]; then
+			LAC=$(printf "%04X" 0x$LAC)" ($(printf "%d" 0x$LAC))"
+			CIDL=$(printf "%08X" 0x$CID)
+			RNC=${CIDL:1:3}
+			CID=${CIDL:4:4}
+			CID="Short $(printf "%X" 0x$CID) ($(printf "%d" 0x$CID)), Long $(printf "%X" 0x$CIDL) ($(printf "%d" 0x$CIDL))"
 		else
 			LAC=""
 		fi
+	else
+		LAC=""
 	fi
 fi
 REGSTAT=$(echo "$REGV" | cut -d, -f2)
 if [ "$REGSTAT" == "5" -a "$COPS" != "-" ]; then
 	COPS_MNC=$COPS_MNC" (Roaming)"
 fi
-if [ -n "$LAC" ]; then
-	LAC_NUM=$(printf "%d" 0x$LAC)
-	LAC_NUM="  ("$LAC_NUM")"
-	CID_NUM=$(printf "%d" 0x$CID)
-	CID_NUM="  ("$CID_NUM")"
-else
+if [ -n "$CID" -a -n "$CID5" -a "$RAT" == "13" ]; then
+	LAC="4G $LAC, 5G $LAC5"
+	CID="4G $CID<br />5G $CID5"
+	RNC="4G $RNC, 5G $RNC5"
+elif [ -n "$CID5" ]; then
+	LAC=$LAC5
+	CID=$CID5
+	RNC=$RNC5
+fi
+if [ -z "$LAC" ]; then
 	LAC="-"
-	LAC_NUM=""
 	CID="-"
 	RNC="-"
-fi
-if [ "$RNC" = "-" ]; then
-	RNC_NUM=""
-else
-	RNC_NUM=$(printf "%d" 0x$RNC)
-	RNC_NUM=" ($RNC_NUM)"
 fi
 
 {
@@ -123,9 +128,9 @@ fi
 	echo 'COPS_MCC="'"$COPS_MCC"'"'
 	echo 'COPS_MNC="'"$COPS_MNC"'"'
 	echo 'LAC="'"$LAC"'"'
-	echo 'LAC_NUM="'"$LAC_NUM"'"'
+	echo 'LAC_NUM="'""'"'
 	echo 'CID="'"$CID"'"'
-	echo 'CID_NUM="'"$CID_NUM"'"'
+	echo 'CID_NUM="'""'"'
 	echo 'RNC="'"$RNC"'"'
-	echo 'RNC_NUM="'"$RNC_NUM"'"'
+	echo 'RNC_NUM="'""'"'
 } > /tmp/cell$CURRMODEM.file
