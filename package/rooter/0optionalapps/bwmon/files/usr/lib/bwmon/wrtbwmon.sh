f35202ee8fe517149c03a308196302b8cb934a89
ROOter <ralph@rfactory.org>
2022-07-28 16:34:44 -0700

    BW fixes
    BW fixes

--- package/rooter/0optionalapps/bwmon/files/usr/lib/bwmon/wrtbwmon.sh
+++ package/rooter/0optionalapps/bwmon/files/usr/lib/bwmon/wrtbwmon.sh
@@ -447,6 +447,10 @@ checkTime()
 		cDay=$pDay
 		cMonth=$pMonth
 		cYear=$pYear
+		monthlyUsageBack="$backPath$cYear-$cMonth-mac_data.js"
+		if [ ! -e $monthlyUsageBack ]; then
+			rm -f $backPath*"mac_data.js"
+		fi
 		rm -f $dataPath[[:digit:]][[:digit:]][[:digit:]][[:digit:]]"-"[[:digit:]][[:digit:]]"-"[[:digit:]][[:digit:]]-daily_data.js
 		rm -f $backPath[[:digit:]][[:digit:]][[:digit:]][[:digit:]]"-"[[:digit:]][[:digit:]]"-"[[:digit:]][[:digit:]]-daily_data.js
 		roll=$(uci -q get custom.bwallocate.rollover)
