6a81c0387eab4f92d9b499f7aa00d254093014b9
ROOter <ralph@rfactory.org>
2022-11-25 13:11:25 -0800

    provider Fix
    provider Fix

--- package/rooter/0splash/ext-splash/files/usr/lib/iframe/stupdate.sh
+++ package/rooter/0splash/ext-splash/files/usr/lib/iframe/stupdate.sh
@@ -300,7 +300,6 @@ if [ $splash = "1" ]; then
 	sed -i -e "s!#MODEM#!$namev!g" $STEMP
 	level2txt "$cops" "single"
 	namev=$(echo "$namev" | tr -d '&')
-	namev="ja! mobil"
 	sed -i -e "s~#PROVIDER#~$namev~g" $STEMP
 	level2txt "$proto" "single"
 	sed -i -e "s!#PROTO#!$namev!g" $STEMP
