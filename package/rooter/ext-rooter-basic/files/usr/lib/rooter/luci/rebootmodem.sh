07e2737a64c2e255f9403bc1eb04f92e28d868ae
ROOter <ralph@rfactory.org>
2023-04-16 10:36:05 -0700

    modem restart
    modem restart

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/rebootmodem.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/rebootmodem.sh
@@ -6,9 +6,16 @@ ROOTER=/usr/lib/rooter
 CURRMODEM=1
 CPORT=$(uci -q get modem.modem$CURRMODEM.commport)
 uVid=$(uci get modem.modem$CURRMODEM.uVid)
-if [ ! -z "$CPORT" ]; then
-	ATCMDD="AT+CFUN=1,1"
-	OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
+if [ $uVid != "2c7c" ]; then
+	if [ ! -z "$CPORT" ]; then
+		ATCMDD="AT+CFUN=1,1"
+		OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
+	fi
+else
+	if [ ! -z "$CPORT" ]; then
+		ATCMDD="AT+QPOWD=0"
+		OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
+	fi
 fi
 CURRMODEM=2
 CPORT=$(uci -q get modem.modem$CURRMODEM.commport)
