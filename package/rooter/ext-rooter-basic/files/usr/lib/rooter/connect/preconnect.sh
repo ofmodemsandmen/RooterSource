442a68cf0834d82d569cc565c2a69bcb418a5c12
ROOter <ralph@rfactory.org>
2023-05-26 16:12:17 -0700

    RM520 restart
    RM520 restart

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/connect/preconnect.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/connect/preconnect.sh
@@ -13,3 +13,11 @@ idP=$(uci -q get modem.modem$CURRMODEM.idP)
 CPORT=$(uci get modem.modem$CURRMODEM.commport)
 
 log "Running PreConnect script"
+
+if [ ! -e /tmp/rst520 ]; then
+	if [ "$idV" = "2c7c" -a "$idP" = "0801" ]; then
+		log "Restart RM520"
+		/usr/lib/rooter/luci/restart.sh $CURRMODEM 11
+		echo "0" > /tmp/rst520
+	fi
+fi
