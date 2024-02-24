67dd8482f5561c9a26e9a3a6d7d87b085da772cd
Dairyman <ralph@rfactory.org>
2024-02-24 10:10:49 -0800

    check connection
    check connection

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/connect/chkconn.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/connect/chkconn.sh
@@ -1,38 +1,6 @@
 #!/bin/sh
 
-ROOTER=/usr/lib/rooter
-
-log() {
-	modlog "Check Connection $CURRMODEM" "$@"
-}
-
-CURRMODEM=$1
-
-watch=$(uci -q get modem.modem$CURRMODEM.watchdog)
-if [ -z "$watch" ]; then
-	watch="0"
-fi
-if [ "$watch" = "0" ]; then
-	log "Check Conn running"
-	if [ -e /usr/lib/autoapn/apn.data ]; then
-		timeout=10
-	else
-		if [ -e /usr/lib/country/mccdata ]; then
-			timeout=10
-		else
-			timeout=7
-		fi
-	fi
-	while [ $timeout -ge 0 ]; do
-		conn=$(uci -q get modem.modem$CURRMODEM.connected)
-		if [ "$conn" = '1' ]; then
-			log "Normal Exit"
-			exit 0
-		fi
-		log "$timeout"
-		timeout=$((timeout-1))
-		sleep 20
-	done
-	log "Modem Restart"
-	$ROOTER/luci/restart.sh $CURRMODEM 11
+result=`ps | grep -i "chkconn1.sh" | grep -v "grep" | wc -l`
+if [ $result -lt 1 ]; then
+	/usr/lib/rooter/connect/chkconn1.sh $1
 fi
