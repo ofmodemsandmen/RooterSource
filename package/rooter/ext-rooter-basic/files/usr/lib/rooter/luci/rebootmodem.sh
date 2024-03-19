450d586c78b3ed05f9abcb42245b6fd8cb0d8b3f
Dairyman <ralph@rfactory.org>
2024-03-19 09:57:26 -0700

    reboot fix
    reboot fix

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/rebootmodem.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/rebootmodem.sh
@@ -2,33 +2,7 @@
 
 ROOTER=/usr/lib/rooter
 
-
-CURRMODEM=1
-CPORT=$(uci -q get modem.modem$CURRMODEM.commport)
-uVid=$(uci get modem.modem$CURRMODEM.uVid)
-if [ $uVid != "2c7c" ]; then
-	if [ ! -z "$CPORT" ]; then
-		ATCMDD="AT+CFUN=1,1"
-		OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
-	fi
-else
-	if [ ! -z "$CPORT" ]; then
-		ATCMDD="AT+QPOWD=0"
-		OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
-	fi
-fi
-CURRMODEM=2
-CPORT=$(uci -q get modem.modem$CURRMODEM.commport)
-uVid=$(uci get modem.modem$CURRMODEM.uVid)
-if [ $uVid != "2c7c" ]; then
-	if [ ! -z "$CPORT" ]; then
-		ATCMDD="AT+CFUN=1,1"
-		OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
-	fi
-else
-	if [ ! -z "$CPORT" ]; then
-		ATCMDD="AT+QPOWD=0"
-		OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
-	fi
-fi
+/usr/lib/rooter/luci/remodem.sh 1 &
+/usr/lib/rooter/luci/remodem.sh 2 &
+sleep 3
 reboot -f
