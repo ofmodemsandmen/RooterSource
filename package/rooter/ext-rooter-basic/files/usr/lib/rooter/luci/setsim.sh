03e5ee8cbeace40b5b115999e6197878b10842b3
Dairyman <ralph@rfactory.org>
2024-05-12 09:20:03 -0700

    dual sim
    dual sim

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/setsim.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/setsim.sh
@@ -3,16 +3,38 @@
 ROOTER=/usr/lib/rooter
 
 snn=$1
-CURRMODEM=1
-CPORT=$(uci -q get modem.modem$CURRMODEM.commport)
 
-ATCMDD="AT+QUIMSLOT?"
-OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
-echo $OX > /tmp/smm
-OX=$(cat /tmp/smm)
-sn=$(echo "$OX" | tr " " "," | cut -d, -f3)
-if [ "$snn" != "$sn" ]; then
-	ATCMDD="AT+QUIMSLOT=$snn"
+bn=$(cat /tmp/sysinfo/board_name)
+bn=$(echo "$bn" | grep "z2101")
+if [ ! -z "$bn" ]; then
+	if [ "$snn" = "2" ]; then
+		snn="0"
+	fi
+	simn=$(uci -q get modem.general.simnum)
+	if [ "$snn" != "$simn" ]; then
+		if [ "$snn" = "1" ]; then
+			echo "1" > /sys/class/gpio/sim/value
+			uci set modem.general.simnum="1"
+			uci commit modem
+		else
+			echo "0" > /sys/class/gpio/sim/value
+			uci set modem.general.simnum="0"
+			uci commit modem
+		fi
+		$ROOTER/luci/restart.sh 1 11
+	fi
+else	
+	CURRMODEM=1
+	CPORT=$(uci -q get modem.modem$CURRMODEM.commport)
+
+	ATCMDD="AT+QUIMSLOT?"
 	OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
-	$ROOTER/luci/restart.sh $CURRMODEM 11
+	echo $OX > /tmp/smm
+	OX=$(cat /tmp/smm)
+	sn=$(echo "$OX" | tr " " "," | cut -d, -f3)
+	if [ "$snn" != "$sn" ]; then
+		ATCMDD="AT+QUIMSLOT=$snn"
+		OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
+		$ROOTER/luci/restart.sh $CURRMODEM 11
+	fi
 fi
--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/simsel.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/luci/simsel.sh
@@ -3,22 +3,35 @@
 ROOTER=/usr/lib/rooter
 
 if [ -e /etc/dualsim ]; then
-	CURRMODEM=1
-	CPORT=$(uci -q get modem.modem$CURRMODEM.commport)
-	if [ ! -z "$CPORT" ]; then
-		ATCMDD="AT+QUIMSLOT?"
-		OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
-		echo $OX > /tmp/smm
-		OX=$(cat /tmp/smm)
-		sn=$(echo "$OX" | tr " " "," | cut -d, -f3)
-		if [ "$sn" != "1" -a "$sn" != "2" ]; then
-			sn="0"
+	bn=$(cat /tmp/sysinfo/board_name)
+	bn=$(echo "$bn" | grep "z2101")
+	if [ ! -z "$bn" ]; then
+		simn=$(cat /sys/class/gpio/sim/value)
+		if [ "$simn" = "1" ]; then
+			echo "1" > /tmp/simsel
+			echo "1" >> /tmp/simsel
+		else
+			echo "1" > /tmp/simsel
+			echo "2" >> /tmp/simsel
 		fi
-		echo "1" > /tmp/simsel
-		echo "$sn" >> /tmp/simsel
 	else
-		echo "0" > /tmp/simsel
-		echo "0" >> /tmp/simsel
+		CURRMODEM=1
+		CPORT=$(uci -q get modem.modem$CURRMODEM.commport)
+		if [ ! -z "$CPORT" ]; then
+			ATCMDD="AT+QUIMSLOT?"
+			OX=$($ROOTER/gcom/gcom-locked "/dev/ttyUSB$CPORT" "run-at.gcom" "$CURRMODEM" "$ATCMDD")
+			echo $OX > /tmp/smm
+			OX=$(cat /tmp/smm)
+			sn=$(echo "$OX" | tr " " "," | cut -d, -f3)
+			if [ "$sn" != "1" -a "$sn" != "2" ]; then
+				sn="0"
+			fi
+			echo "1" > /tmp/simsel
+			echo "$sn" >> /tmp/simsel
+		else
+			echo "0" > /tmp/simsel
+			echo "0" >> /tmp/simsel
+		fi
 	fi
 else
 	echo "0" > /tmp/simsel
