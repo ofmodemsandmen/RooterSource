d971ef1544447ea68842d5b47446c8f99a9db58f
Dairyman <ralph@rfactory.org>
2024-08-27 15:42:42 -0700

    sched reboot
    sched reboot

--- package/rooter/0basicapps/ext-extra/files/usr/lib/rooter/luci/schedreboot.sh
+++ package/rooter/0basicapps/ext-extra/files/usr/lib/rooter/luci/schedreboot.sh
@@ -4,5 +4,5 @@ ROOTER=/usr/lib/rooter
 
 /usr/lib/rooter/luci/restartrun.sh 1 &
 /usr/lib/rooter/luci/restartrun.sh 2 &
-sleep 3
-touch /etc/banner && reboot -f
+sleep 70 && touch /etc/banner
+reboot -f
