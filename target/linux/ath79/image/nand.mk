6584f5280fdb251d6f814f2b40a64a28c3bf0048
Dairyman <ralph@rfactory.org>
2025-08-24 06:39:45 -0700

    add x1200
    add x1200

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -11,3 +11,27 @@ define Device/glinet_gl-ar300m-nand
   IMAGE/factory.ubi := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
 endef
 #TARGET_DEVICES += glinet_gl-ar300m-nand
+
+define Device/glinet_gl-x1200-nor
+  ATH_SOC := qca9563
+  DEVICE_TITLE := GL.iNet GL-X1200 (NOR)
+  DEVICE_PACKAGES := kmod-usb2 kmod-ath10k-ct ath10k-firmware-qca9887-ct-htt block-mount
+  IMAGE_SIZE := 16000k
+  SUPPORTED_DEVICES += gl-x1200 glinet,gl-x1200
+endef
+TARGET_DEVICES += glinet_gl-x1200-nor
+
+define Device/glinet_gl-x1200-nor-nand
+  ATH_SOC := qca9563
+  DEVICE_TITLE := GL.iNet GL-X1200 (NOR/NAND)
+  DEVICE_PACKAGES := kmod-usb2 kmod-ath10k-ct ath10k-firmware-qca9887-ct-htt block-mount PCI_SUPPORT
+  KERNEL_SIZE := 2048k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  VID_HDR_OFFSET := 2048
+  IMAGES := factory.img sysupgrade.tar
+  IMAGE/sysupgrade.tar := sysupgrade-tar | append-metadata
+  IMAGE/factory.img := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | append-metadata
+  SUPPORTED_DEVICES += gl-x1200 glinet,gl-x1200
+endef
+TARGET_DEVICES += glinet_gl-x1200-nor-nand
