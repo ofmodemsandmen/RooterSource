2891b65af3c753cf6dd83a54b125f77815030b1f
Dairyman <ralph@rfactory.org>
2025-09-05 12:38:30 -0700

    T99W175 Network Status
    T99W175 Network Status

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/common/t77data.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/common/t77data.sh
@@ -20,8 +20,12 @@ OXC=$(echo $OX | tr -d " ")
 #OXC='AT+CSQ+CSQ:12,99OKAT+COPS?+COPS:0,2,"302490",7OKAT^CA_INFO?LTESERVINGINFORMATION:PCCINFO:BANDISLTE_B13,BAND_WIDTHIS5.0MHZOKAT^DEBUG?RAT:LTEMCC:302,MNC:490LTE_CELL_ID:103729697LTE_TAC:60400LTE_TX_PWR:NARX_DIVERSITY:3(-114.8DBM,-140.0DBM,NA,NA)PCELL:LTE_BAND:13LTE_BAND_WIDTH:5.0MHZCHANNEL:5205PCI:290LTE_RSRP:-115.0DBM,RSRQ:-10.7DBLTE_RSSI:-90.2DBM,LTE_SNR:2.4DBSCELL:LTE_BAND:3LTE_BAND_WIDTH:20.0MHZCHANNEL:1575PCI:2LTE_RSRP:-74.9DBM,RSRQ:-10.4DBMLTE_RSSI:-44.6DBM,LTE_SNR:30.0DBMSCELL:LTE_BAND:7LTE_BAND_WIDTH:20.0MHZCHANNEL:3100PCI:3LTE_RSRP:-75.1DBM,RSRQ:-10.4DBMLTE_RSSI:-44.9DBM,LTE_SNR:30.0DBMNR_BAND:N78NR_BAND_WIDTH:100.0MHZNR_CHANNEL:636666NR_PCI:4NR_RSRP:82DBMRX_DIVERSITY:15(-82.1DBM,-110.6DBM,-140DBM,-125.8DBM)NR_RSRQ:-11DBNR_SNR:16.5DBOKAT$DEBUG?ERRORAT^SYSCONFIG?^SYSCONFIG:17,0,1,2OKAT$QCSQ$QCSQ:-125,-2,0,0,-90OKAT+TEMP?ERRORAT^TEMP?PA:44CSKINSENSOR:44CTSENS:46COK'
 
 #OXC='AT+CSQ+CSQ:25,99OKAT+COPS?+COPS:0,2,"25002",7OKAT^CA_INFO?LTEservinginformation:PCCinfo:BandisLTE_B1, Band_widthis10.0MHzSCC1 info: Band is LTE_B7, Band_width is 20.0 MHzSCC2 info: Band is LTE_B7, Band_width is 20.0 MHzSCC3 info: Band is LTE_B3, Band_width is 15.0 MHzOKAT^DEBUG?RAT:LTEmcc:250,mnc:02lte_cell_id:90127718lte_tac:3556lte_tx_pwr:8.0dBmlte_ant_rsrp:rx_diversity:1 (-85.6dBm,NA,NA,NA)pcell: lte_band:1 lte_band_width:10.0MHzchannel:200 pci:464lte_rsrp:-85.7dBm,rsrq:-7.1dBlte_rssi:-60.3dBm,lte_snr:25.2dBscell: lte_band:7 lte_band_width:20.0MHzchannel:3048 pci:47lte_rsrp:-92.0dBm,rsrq:-11.0dBlte_rssi:-71.2dBm,lte_snr:0.0dBscell: lte_band:7 lte_band_width:20.0MHzchannel:2850 pci:47lte_rsrp:-91.2dBm,rsrq:-8.7dBlte_rssi:-73.7dBm,lte_snr:0.0dBscell: lte_band:3 lte_band_width:15.0MHzchannel:1575 pci:135lte_rsrp:-75.9dBm,rsrq:-8.0dBlte_rssi:-59.0dBm,lte_snr:0.0dBOKAT$DEBUG?ERRORAT^SYSCONFIG?^SYSCONFIG: 2,0,1,2OKAT$QCSQ$QCSQ: -125,-2,0,0,-64OKAT+TEMP?ERRORAT^TEMP?PA: 26CSkin Sensor: 26CTSENS: 27COK'
+
+#OXC='AT+CSQ+CSQ: 31,99OKAT+COPS?+COPS: 0,2,"311480",13OKAT^CA_INFO?LTESERVINGINFORMATION:PCCINFO:BANDISLTE_B66,BAND_WIDTHIS20.0MHZSCC1INFO:BANDISLTE_B2,BAND_WIDTHIS10.0MHZPCCINFO:BANDISNR5G_N5,BAND_WIDTHIS10.0MHZOKAT^DEBUG?RAT:LTE+NRMCC:311,MNC:480LTE_CELL_ID:85245994LTE_TAC:8198LTE_TX_PWR:-19.0DBMLTE_ANT_RSRP:RX_DIVERSITY:15 (-256.0DBM,-57.9DBM,-256.0DBM,-256.0DBM)PCELL:LTE_BAND:66LTE_BAND_WIDTH:20.0MHZCHANNEL:1000 PCI:35LTE_RSRP:-58.4DBM,RSRQ:-7.8DBLTE_RSSI:-30.6DBM,LTE_SNR:13.4DBSCELL:LTE_BAND:2LTE_BAND_WIDTH:10.0MHZCHANNEL:1150PCI:35LTE_RSRP:-58.3DBM,RSRQ:-12.1DBLTE_RSSI:-37.9DBM,LTE_SNR:17.4DBNR_BAND:N5NR_BAND_WIDTH:10.0MHZNR_CHANNEL:177150NR_PCI:25NR_RSRP:-52.2DBM RX_DIVERSITY:3(-51.3DBM,-56.5DBM,-44.0DBM,-44.0DBM)NR_RSRQ:-11.1DBNR_SNR:14.0DBOKAT$DEBUG?ERRORAT^SYSCONFIG?^SYSCONFIG: 17,2,1,2OKAT$QCSQ$QCSQ:-125,-2,0,0,-31OKAT+TEMP?ERRORAT^TEMP?PA:40CSKINSENSOR:41CTSENS: 43COK'
+#OX=$OXC
 #OXC=$(echo $OXC | tr 'a-z' 'A-Z')
 #OXC=$(echo $OXC | tr -d " ")
+
 
 PCELL_LTE="RAT:[LTE+NR]\{3,6\}MCC:.\+PCELL:LTE_BAND:[0-9]\+[^C]\+WIDTH:[.0-9]\{1,6\}MHZCHANNEL:[0-9]\+PCI:[0-9]\{1,3\}LTE_RSRP:.\{2,8\}DBM,RSRQ:.\{2,8\}DB.\{12,20\},LTE_SNR:.\{3,7\}DB"
 PXELL_LTE="RAT:LTEMCC:.\+PCELL:LTE_BAND:[0-9]\{1,2\}\+LTE_BAND_WIDTH:[.0-9]\{1,6\}MHZCHANNEL:[0-9]\+PCI:[0-9]\{1,3\}RSRP:.\{2,8\}DBM,RSRQ:.\{2,8\}DB.\{12,20\},SNR:.\{3,7\}DB"
@@ -88,6 +92,7 @@ TECH="${TECH: -1}"
 
 PLTE=$(echo $OXC | grep -o "$PCELL_LTE")
 PXLTE=$(echo $OXC | grep -o "$PXRELL_LTE")
+
 if [ -z "$PXLTE" ]; then
 	PXLTE=$(echo $OXC | grep -o "$PXRELL_LTE1")
 	if [ -z "$PXLTE" ]; then
@@ -102,6 +107,7 @@ if [ "$idV" = 1e2d ]; then
 		PXLTE=""
 	fi
 fi
+
 #SLTEX=$(echo $OXC | grep -o "SCELL")
 #if [ ! -z "$SLTEX" ]; then
 #	log "$OXC"
@@ -161,20 +167,39 @@ fi
 
 if [ -n "$PXLTE" ]; then
 	MODE="LTE"
-	LBANDX=$(echo $PXLTE | cut -d: -f10 | grep -o "[0-9]\{1,3\}")
-	BW=$(echo $PXLTE | cut -d: -f11 | grep -o "[.0-9]\{3,5\}")
+	EXTR=$(echo $PXLTE | grep -o "LTE_ANT_RSRP:RX_DIVERSITY")
+	if [ -z "$EXTR" ]; then
+		LBANDX=$(echo $PXLTE | cut -d: -f10 | grep -o "[0-9]\{1,3\}")
+		BW=$(echo $PXLTE | cut -d: -f11 | grep -o "[.0-9]\{3,5\}")
+	else
+		LBANDX=$(echo $PXLTE | cut -d: -f11 | grep -o "[0-9]\{1,3\}")
+		BW=$(echo $PXLTE | cut -d: -f12 | grep -o "[.0-9]\{3,5\}")
+	fi
 	if [ ${BW: -2} == ".0" ]; then
 		BW=$(printf "%.0f" $BW)
 	fi
 	LBAND="B"$LBANDX" (Bandwidth $BW MHz)"
-	CHANNEL=$(echo $PXLTE | cut -d: -f12 | grep -o "[0-9]\{1,7\}")
+	if [ -z "$EXTR" ]; then
+		CHANNEL=$(echo $PXLTE | cut -d: -f12 | grep -o "[0-9]\{1,7\}")
+	else
+		CHANNEL=$(echo $PXLTE | cut -d: -f13 | grep -o "[0-9]\{1,7\}")
+	fi
+	
 	if [ "$LBANDX" -gt 20 ]; then
 		let CHANNEL=$CHANNEL+65536
 	fi
-	PCI=$(echo $PXLTE | cut -d: -f13 | grep -o "[0-9]\{1,3\}")
-	RSCP=$(printf "%.0f" $(echo $PXLTE | cut -d: -f14 | grep -o "[-.0-9]\{2,7\}"))
-	ECIO=$(printf "%.0f" $(echo $PXLTE | cut -d: -f15 | grep -o "[-.0-9]\{2,6\}"))
-	SINR=$(printf "%.0f" $(echo $PXLTE | cut -d: -f17 | grep -o "[-.0-9]\{3,6\}"))
+	if [ -z "$EXTR" ]; then
+		PCI=$(echo $PXLTE | cut -d: -f13 | grep -o "[0-9]\{1,3\}")
+		RSCP=$(printf "%.0f" $(echo $PXLTE | cut -d: -f14 | grep -o "[-.0-9]\{2,7\}"))
+		ECIO=$(printf "%.0f" $(echo $PXLTE | cut -d: -f15 | grep -o "[-.0-9]\{2,6\}"))
+		SINR=$(printf "%.0f" $(echo $PXLTE | cut -d: -f17 | grep -o "[-.0-9]\{3,6\}"))
+	else
+		PCI=$(echo $PXLTE | cut -d: -f14 | grep -o "[0-9]\{1,3\}")
+		RSCP=$(printf "%.0f" $(echo $PXLTE | cut -d: -f15 | grep -o "[-.0-9]\{2,7\}"))
+		ECIO=$(printf "%.0f" $(echo $PXLTE | cut -d: -f16 | grep -o "[-.0-9]\{2,6\}"))
+		SINR=$(printf "%.0f" $(echo $PXLTE | cut -d: -f18 | grep -o "[-.0-9]\{3,6\}"))
+
+	fi
 	if [ -n "$PNR" ]; then
 		MODE="LTE+NR (NR5G-NSA)"
 		BAND="n"$(echo $PNR | cut -d: -f2 | grep -o "[0-9]\{1,3\}")
@@ -209,6 +234,7 @@ if [ -n "$PXLTE" ]; then
 			SINR=$SINR",-"
 		fi
 	done
+
 fi
 
 if [ -z "$PLTE" ] && [ -n "$TECH" ] && [ -z "$PXLTE" ]; then
